a76b6388e2dad77410f6f323649e33775f7d9025
Paul Hebble <pjhebble@gmail.com>
2020-12-16 16:03:25 -0600

    Code in __init__

---
+++ inflate_netkan/__init__.py
@@ -0,0 +1,20 @@
+import boto3
+from os import environ
+from git import Repo
+from exitstatus import ExitStatus
+import logging
+
+from netkan.repos import CkanMetaRepo
+from netkan.common import netkans, sqs_batch_entries
+
+
+def inflate_netkan() -> None:
+    messages = (nk.sqs_message(CkanMetaRepo(Repo('CKAN-meta')).highest_version(nk.identifier))
+                for nk in netkans('NetKAN', [environ['INPUT_IDENTIFIER']]))
+    for batch in sqs_batch_entries(messages):
+        logging.info(f'Queueing inflation request batch: {batch}')
+        boto3.client('sqs').send_message_batch(
+            QueueUrl=boto3.resource('sqs').get_queue_by_name(QueueName='NetKANInbound').url,
+            Entries=batch
+        )
+    exit(ExitStatus.success)
